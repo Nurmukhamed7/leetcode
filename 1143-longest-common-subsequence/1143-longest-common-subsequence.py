e314ece5648ab5956312816cee017eae5924c3db
Nurmukhamed Berikbolov <nurik.berikbolov@gmail.com>
2024-07-30 00:19:25 +0200

    Time: 448 ms (82.74%), Space: 41.7 MB (64.01%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,14 +1,15 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        cache = [[0 for i in range(0, len(text1) + 1)] for i in range (0, len(text2) + 1)]
-        for s2Row in range(0,len(text2)+1):
-            for s1Col in range(0,len(text1)+1):
-                print(cache[s2Row][s1Col])
-                if (s2Row == 0 or s1Col == 0) :
-                    cache[s2Row][s1Col]=0
-                elif (text2[s2Row-1] == text1[s1Col-1]):
-                    cache[s2Row][s1Col] = cache[s2Row - 1][s1Col - 1] + 1
-                else :
-                    cache[s2Row][s1Col] = max(cache[s2Row - 1][s1Col], cache[s2Row][s1Col - 1])
-
-        return cache[len(text2)][len(text1)]
+        len1, len2 = len(text1), len(text2)
+        
+        dp = [[0]*(len2+1) for _ in range(len1+1)]
+        
+        for i in range(1, len1+1):
+            for j in range(1, len2+1):
+                if text1[i-1] == text2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]+1
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+                    
+        return dp[len1][len2]
+                
